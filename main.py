05056a569a992305cad2cbfb6a4525c5ab76a986
otomarukanta <kanta208@gmail.com>
2019-02-20 03:50:35 +0900

    use jrdb lib

--- main.py
+++ main.py
@@ -1,52 +1,21 @@
 import os
-import requests
-import zlib
-import re
-import io
-import zipfile
 import base64
-from jrdbparser import JRDBParser, JRDBAvroWriter, schema
-from google.cloud import storage
-
-re_schema_type = re.compile(r'([A-Z]*?)([0-9]*?)\.txt')
-parsers = {
-    'kab': JRDBParser(schema.kab)
-}
-writers = {
-    'kab': JRDBAvroWriter(schema.kab)
-}
-
-
-def parse(session, url, bucket):
-    res = session.get(url, stream=True)
-    z = zipfile.ZipFile(io.BytesIO(res.content))
-
-    for filename in z.namelist():
-        schema_type, yymmdd = re_schema_type.findall(filename)[0]
-        schema_type = schema_type.lower()
-        if schema_type not in parsers:
-            print('Does not support schema type. [{}]'.format(schema_type))
-            continue
-
-        with z.open(filename) as f:
-            records = [parsers[schema_type].parse(x)
-                       for x in f.read().split(b'\r\n') if x]
-            print('Success. [{}]'.format(url))
-            buf = io.BytesIO()
-            writers[schema_type].write(buf, records)
-            blob = bucket.blob(f'{schema_type}/{yymmdd}')
-            blob.upload_from_string(buf.getvalue())
+import time
+from jrdb.client import JRDBClient
+from jrdb.repo import JRDBDataGCSRepo
+from jrdb import urlcodec
+from logging import basicConfig, DEBUG
+basicConfig(level=DEBUG)
 
 
 def main(data, context):
-    session = requests.Session()
-    session.mount('http://', requests.adapters.HTTPAdapter(max_retries=3))
     auth = (os.environ['JRDB_ID'], os.environ['JRDB_PW'])
-    session.auth = auth
+    client = JRDBClient(auth)
+    repo = JRDBDataGCSRepo(os.environ['OUTPUT_BUCKET_NAME'])
+    urls = urlcodec.decode(base64.b64decode(data['data']))
 
-    gcs_cli = storage.Client()
-    bucket = gcs_cli.get_bucket(os.environ['OUTPUT_BUCKET_NAME'])
-
-    urls = zlib.decompress(base64.b64decode(data['data'])).decode('utf-8').split(',')
     for url in urls:
-        parse(session, url, bucket)
+        jrdb_data_list = client.fetch_jrdbdata(url)
+        for jrdb_data in jrdb_data_list:
+            repo.store(jrdb_data)
+        time.sleep(1)
